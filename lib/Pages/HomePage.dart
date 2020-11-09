4ceae5f9ff950fe81ad3a9263597b7623df85454
ln2dark <pedrombcruz47@gmail.com>
2020-11-09 11:36:21 +0000

    Initial

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:telegramchatapp/Pages/ChattingPage.dart';
+import 'package:telegramchatapp/main.dart';
 import 'package:telegramchatapp/models/user.dart';
 import 'package:telegramchatapp/Pages/AccountSettingsPage.dart';
 import 'package:telegramchatapp/Widgets/ProgressWidget.dart';
@@ -13,17 +15,75 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 
 class HomeScreen extends StatefulWidget {
+  final String firebaseUserID;
+  HomeScreen({Key key, @required this.firebaseUserID}) : super(key: key);
   @override
   State createState() => HomeScreenState();
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchTextEditingController = TextEditingController();
+
+  homePageHeader(){
+    return AppBar(
+      automaticallyImplyLeading: false, // remove back button
+      actions: <Widget> [
+        IconButton(
+          icon: Icon(Icons.settings, size: 30.0, color: Colors.white),
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Settings()));
+
+          },
+        ),
+      ],
+      backgroundColor: Colors.lightBlue,
+      title: Container(
+        margin: new EdgeInsets.only(bottom: 4.0),
+        child: TextFormField(
+          style: TextStyle(fontSize: 18.0, color: Colors.white),
+          controller: searchTextEditingController,
+          decoration: InputDecoration(
+            hintText: "Procurar...",
+            hintStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            filled: true,
+            prefixIcon: Icon(Icons.person_pin, color: Colors.white, size: 30.0),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.clear, color: Colors.white),
+              onPressed: emptyTextFormField,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  emptyTextFormField(){
+    searchTextEditingController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: homePageHeader(),
+      body:  RaisedButton.icon(onPressed: logoutUser, icon: Icon(Icons.logout), label: Text("Sign Out")),
+    );
 
   }
-}
 
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  Future<Null> logoutUser() async{
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.disconnect();
+    await googleSignIn.signOut();
+    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => MyApp()), (Route<dynamic> route) => false);
+  }
+}
 
 class UserResult extends StatelessWidget
 {
--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -11,13 +11,42 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 class LoginScreen extends StatefulWidget {
-
+  LoginScreen({Key key }) : super(key: key);
   @override
   LoginScreenState createState() => LoginScreenState();
 }
 
 class LoginScreenState extends State<LoginScreen> {
   @override
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences _preferences;
+  bool isLoggedIn = false;
+  bool isLoading = false;
+  FirebaseUser _firebaseUser;
+
+  @override
+  void intState(){
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async{
+    this.setState(() {
+      isLoggedIn = true;
+    });
+    _preferences = await SharedPreferences.getInstance();
+    isLoggedIn = await googleSignIn.isSignedIn();
+    if(isLoggedIn){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(firebaseUserID: _preferences.getString("id"))));
+    }
+    this.setState(() {
+      isLoading = false;
+    });
+
+  }
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -40,6 +69,7 @@ class LoginScreenState extends State<LoginScreen> {
               ),
             ),
             GestureDetector(
+              onTap: controlSignIn,
               child: Center(
                 child: Column(
                   children: <Widget>[
@@ -55,7 +85,7 @@ class LoginScreenState extends State<LoginScreen> {
                     ),
                     Padding(
                       padding: EdgeInsets.all(1.0),
-                      child: circularProgress(),
+                      child: isLoading ? circularProgress() : Container(),
                     ),
                   ],
                 ),
@@ -66,6 +96,53 @@ class LoginScreenState extends State<LoginScreen> {
       ),
     );
 
+  }
+
+  Future<Null> controlSignIn() async{
+    _preferences = await SharedPreferences.getInstance();
+    this.setState(() {
+      isLoading = true;
+    });
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication authentication = await googleSignInAccount.authentication;
+    final AuthCredential credential  = GoogleAuthProvider.getCredential(idToken: authentication.idToken, accessToken: authentication.accessToken);
+    FirebaseUser firebaseUser = (await firebaseAuth.signInWithCredential(credential)).user;
+    if(firebaseUser != null){
+      final QuerySnapshot resultQuery = await Firestore.instance.collection("users").where("id", isEqualTo: firebaseUser.uid).getDocuments();
+      final List<DocumentSnapshot> documentSnapshot  = resultQuery.documents;
+      if(documentSnapshot.length == 0){
+        Firestore.instance.collection("users").document(firebaseUser.uid).setData(
+            {
+              "nickname" : firebaseUser.displayName,
+              "photoUrl" : firebaseUser.photoUrl,
+              "id" : firebaseUser.uid,
+              "aboutMe" : "Sobre Mim",
+              "createdAt" : DateTime.now().millisecondsSinceEpoch.toString(),
+              "chattingWith" : null,
+            });
+        _firebaseUser = firebaseUser;
+        await _preferences.setString("id", _firebaseUser.uid);
+        await _preferences.setString("nickname", _firebaseUser.displayName);
+        await _preferences.setString("photoUrl", _firebaseUser.photoUrl);
+      }else{
+        _firebaseUser = firebaseUser;
+        await _preferences.setString("id", documentSnapshot[0]["id"]);
+        await _preferences.setString("nickname", documentSnapshot[0]["nickname"]);
+        await _preferences.setString("photoUrl", documentSnapshot[0]["photoUrl"]);
+        await _preferences.setString("aboutMe", documentSnapshot[0]["aboutMe"]);
+      }
+      Fluttertoast.showToast(msg: "Logged in");
+      this.setState(() {
+        isLoading = false;
+      });
+      Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(firebaseUserID: firebaseUser.uid)));
+    }else{
+      Fluttertoast.showToast(msg: "Tenta outra vez");
+      this.setState(() {
+        isLoading = false;
+      });
+
+    }
   }
 
 }
